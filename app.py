9991afb463a841b61e92d7df9c2d4dd37e5369d2
AEGIR24 <160857424+AEGIR24@users.noreply.github.com>
2024-02-22 07:34:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,19 @@ st.write("Facilmente puedo realizar backend y frontend")
 image = Image.open('mago2.jpg')
 
 st.image(image, caption = 'mago')
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
+
+st.subheader("AHora usamos 2 columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp:
+    st.write('Correcto!')
+
+
