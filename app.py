0c0f2c4a2058a48a7e4a56d8f11dac3719dbdf49
AEGIR24 <160857424+AEGIR24@users.noreply.github.com>
2024-02-22 07:49:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,15 @@ with col1:
 
 with col2:
   st.subheader("Esta es la segunda columna:")
+  modo = st.radio("Que modalidad es la principal en tu interfaz", ('Visual', 'Auditiva', 'Táctil'))
+  if modo == 'Visual':
+    st.write('La vista es fundamental para tu interfaz')
+  if modo == 'Auditiva':
+    st.write('La audición es fundamental para tu interfaz')
+  if modo == 'Táctil':
+    st.write('El tacto es fundamental para tu interfaz')
+
+st.subheader("Uso de botones")
   
 
 
