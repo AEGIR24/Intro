c01a43e8ebef6c0b7fa3054a687bcb8a4343d672
AEGIR24 <160857424+AEGIR24@users.noreply.github.com>
2024-02-22 07:23:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 from PIL import Image
 
 st.title("Mi primera app!!")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para intertfaces multimodales")
+st.write("Facilmente puedo realizar backend y frontend")
+image = Image.open('mago2.jpg')
